b07a83fb8c0ce721aafaf79b62917e97d8634446
Matheus Abreu <matheusa596@gmail.com>
2018-07-11 19:00:11 -0300

    Atualização do script com as coisas do Duarte.

--- Comp3/src/br/sisfarj/ccomp/bd/sisfarjdb.sql
+++ Comp3/src/br/sisfarj/ccomp/bd/sisfarjdb.sql
@@ -2,14 +2,15 @@ CREATE SCHEMA comp3;
 SET SCHEMA comp3;
 --
 CREATE TABLE Associacao (
- matriculaAssociacao INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1000000, INCREMENT BY 1),
+ matriculaAssociacao INT PRIMARY KEY,
  nome VARCHAR(50) NOT NULL,
  telefone VARCHAR(20) NOT NULL,
  sigla VARCHAR(5) NOT NULL,
  endereco VARCHAR(200) NOT NULL,
  numeroPagamento INT NOT NULL,
  numeroOficio INT NOT NULL,
- dataOficio TIMESTAMP NOT NULL
+ dataOficio TIMESTAMP NOT NULL,
+ temAcesso CHAR(1) NOT NULL DEFAULT 'F' CHECK (temAcesso IN ('T', 'F'))
 );
 
 CREATE TABLE Atleta (
@@ -26,8 +27,8 @@ CREATE TABLE Atleta (
 CREATE TABLE LocalCompeticao (
  endereco VARCHAR(300) NOT NULL,
  nome VARCHAR(50) NOT NULL,
- piscina25 BOOLEAN NOT NULL,
- piscina50 BOOLEAN NOT NULL
+ piscina25 CHAR(1) CHECK (piscina25 IN ('T', 'F')),
+ piscina50 CHAR(1) CHECK (piscina50 IN ('T', 'F'))
 );
 
 ALTER TABLE LocalCompeticao ADD CONSTRAINT PK_LocalCompeticao PRIMARY KEY (endereco);
@@ -40,74 +41,49 @@ CREATE TABLE Competicao (
 
 ALTER TABLE Competicao ADD CONSTRAINT PK_Competicao PRIMARY KEY (dataCompeticao,endereco);
 
-
 CREATE TABLE Pessoa (
- matricula INT PRIMARY KEY,
+ matricula INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1000000, INCREMENT BY 1),
  senha VARCHAR(15) NOT NULL,
  tipo VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE TecnicoAssociacao (
- matriculaAssociacao INT,
- matricula INT
-);
-
-
 CREATE TABLE Prova (
- nomeProva VARCHAR(30) NOT NULL,
- classe VARCHAR(30) NOT NULL,
- categoria VARCHAR(30) NOT NULL
-);
-
-ALTER TABLE Prova ADD CONSTRAINT PK_Prova PRIMARY KEY (nomeProva,classe,categoria);
-
-CREATE TABLE AtletaProva (
- matriculaAtleta INT NOT NULL,
- nomeProva VARCHAR(30) NOT NULL,
- classe VARCHAR(30) NOT NULL,
- categoria VARCHAR(30) NOT NULL,
- tempo TIME,
- pontuacao INT
-);
-
-ALTER TABLE AtletaProva ADD CONSTRAINT PK_AtletaProva PRIMARY KEY (nomeProva,classe,categoria,matriculaAtleta);
-ALTER TABLE AtletaProva ADD CONSTRAINT FK_AtletaProvaAtleta FOREIGN KEY (matriculaAtleta) REFERENCES Atleta (matriculaAtleta);
-ALTER TABLE AtletaProva ADD CONSTRAINT FK_AtletaProvaProva FOREIGN KEY (nomeProva,classe,categoria) REFERENCES Prova (nomeProva,classe,categoria);
-
-
-CREATE TABLE CompeticaoProva (
- nomeProva VARCHAR(30) NOT NULL,
- classe VARCHAR(30) NOT NULL,
- categoria VARCHAR(30) NOT NULL,
+ tipoPiscina VARCHAR(5) NOT NULL CHECK (tipoPiscina IN ('50M', '25M')),
+ nomeProva VARCHAR(30) NOT NULL CHECK (nomeProva IN ('50L', '100L', '200L', '400L',
+ '800L', '1500L', '50C', '100C', '200C', '50P', '100P', '200P', '50B', '100B', '200B',
+ '200M', '400M')),
+ classe VARCHAR(30) NOT NULL CHECK(classe IN ('MIRIM', 'MIRIM_I_II', 'PETIZ_I_II', 
+ 'INFANTIL_I_II', 'JUVENIL_I_II', 'JUNIOR_I_II', 'SENIOR', 'MASTER', 'ABSOLUTO', 'INCULADO')),
+ categoria VARCHAR(30) NOT NULL CHECK(categoria IN ('FEMININO', 'MASCULINO')),
  dataCompeticao TIMESTAMP NOT NULL,
  endereco VARCHAR(300) NOT NULL
 );
 
-ALTER TABLE CompeticaoProva ADD CONSTRAINT PK_CompeticaoProva PRIMARY KEY (nomeProva,classe,categoria,dataCompeticao,endereco);
-ALTER TABLE CompeticaoProva ADD CONSTRAINT FK_CompeticaoProvaCompeticao FOREIGN KEY (dataCompeticao,endereco) REFERENCES Competicao (dataCompeticao,endereco);
-ALTER TABLE CompeticaoProva ADD CONSTRAINT FK_CompeticaoProvaProva FOREIGN KEY (nomeProva,classe,categoria) REFERENCES Prova (nomeProva,classe,categoria);
+CREATE TABLE AtletaProva (
+ matriculaAtleta INT NOT NULL,
+ nomeProva VARCHAR(30) NOT NULL,
+ tipoPiscina VARCHAR (5) NOT NULL,
+ classe VARCHAR(30) NOT NULL,
+ categoria VARCHAR(30) NOT NULL,
+ dataCompeticao TIMESTAMP NOT NULL,
+ endereco VARCHAR(300) NOT NULL,
+ tempo TIME,
+ pontuacao INT
+);
 
+ALTER TABLE Associacao ADD CONSTRAINT FK_AssociacaoPessoa FOREIGN KEY (matriculaAssociacao) REFERENCES Pessoa (matricula);
 ALTER TABLE Atleta ADD CONSTRAINT FK_Atleta_0 FOREIGN KEY (matriculaAssociacao) REFERENCES Associacao (matriculaAssociacao);
-
-
-ALTER TABLE TecnicoAssociacao ADD CONSTRAINT FK_TecnicoAssociacao_0 FOREIGN KEY (matriculaAssociacao) REFERENCES Associacao (matriculaAssociacao);
-ALTER TABLE TecnicoAssociacao ADD CONSTRAINT FK_TecnicoPessoa FOREIGN KEY (matricula) REFERENCES Pessoa (matricula);
-
-
 ALTER TABLE Competicao ADD CONSTRAINT FK_Competicao_0 FOREIGN KEY (endereco) REFERENCES LocalCompeticao (endereco);
 
+ALTER TABLE Prova ADD CONSTRAINT PK_Prova PRIMARY KEY (tipoPiscina, nomeProva,classe,categoria,dataCompeticao,endereco);
+ALTER TABLE Prova ADD CONSTRAINT FK_ProvaCompeticao FOREIGN KEY (dataCompeticao,endereco) REFERENCES Competicao (dataCompeticao,endereco);
 
-INSERT INTO COMP3.PESSOA values (780252, 'Pl2252122*', 'DIRETOR');
-INSERT INTO COMP3.PESSOA values (123, '123', 'DIRETOR');
+ALTER TABLE AtletaProva ADD CONSTRAINT PK_AtletaProva PRIMARY KEY (tipoPiscina, nomeProva,classe,categoria, dataCompeticao, endereco,matriculaAtleta);
+ALTER TABLE AtletaProva ADD CONSTRAINT FK_AtletaProvaAtleta FOREIGN KEY (matriculaAtleta) REFERENCES Atleta (matriculaAtleta);
+ALTER TABLE AtletaProva ADD CONSTRAINT FK_AtletaProvaProva FOREIGN KEY (tipoPiscina, nomeProva,classe,categoria, dataCompeticao, endereco) REFERENCES Prova (tipoPiscina, nomeProva,classe,categoria, dataCompeticao, endereco);
 
-ALTER TABLE comp3.localcompeticao
-DROP COLUMN piscina25;
-ALTER TABLE comp3.localcompeticao
-DROP COLUMN piscina50;
 
-ALTER TABLE comp3.localcompeticao
-ADD COLUMN piscina25 CHAR(1) NOT NULL DEFAULT 'F' CHECK (piscina25 IN ('T', 'F'));
-ALTER TABLE comp3.localcompeticao
-ADD COLUMN piscina50 CHAR(1) NOT NULL DEFAULT 'F' CHECK (piscina50 IN ('T', 'F'));
+INSERT INTO COMP3.PESSOA (SENHA, TIPO) values ('12345', 'DIRETOR');
+--INSERT INTO COMP3.PESSOA values ('123', 'DIRETOR');
 
-insert into comp3.localcompeticao values ('Barra', 'BarraPool', 'T', 'T'), ('Piedade', 'PiedadePool', 'F', 'F');
+--insert into comp3.localcompeticao values ('Barra', 'BarraPool', 'T', 'T'), ('Piedade', 'PiedadePool', 'F', 'F');
