c7b4105a56929543ae0809cc0745855f16d231a3
Matheus Abreu <matheusa596@gmail.com>
2018-07-11 13:52:05 -0300

    Atualização do formato da data.

--- Comp3/src/br/sisfarj/ccomp/aplicacao/Constantes.java
+++ Comp3/src/br/sisfarj/ccomp/aplicacao/Constantes.java
@@ -2,5 +2,5 @@ package br.sisfarj.ccomp.aplicacao;
 
 public class Constantes {
 	public static final String ERRO = "erro";
-	public static final String FORMATO_DATA = "yyyy-mm-dd";
+	public static final String FORMATO_DATA = "yyyy-MM-dd";
 }
