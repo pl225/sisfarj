f7f97061f7eb04fe66d629eed098e27f739153d7
Matheus Abreu <matheusa596@gmail.com>
2018-07-12 09:48:03 -0300

    Atualização do script do BD

--- Comp3/src/br/sisfarj/ccomp/bd/sisfarjdb.sql
+++ Comp3/src/br/sisfarj/ccomp/bd/sisfarjdb.sql
@@ -2,7 +2,7 @@ CREATE SCHEMA comp3;
 SET SCHEMA comp3;
 --
 CREATE TABLE Associacao (
- matriculaAssociacao INT PRIMARY KEY,
+ matriculaAssociacao INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1000000, INCREMENT BY 1),
  nome VARCHAR(50) NOT NULL,
  telefone VARCHAR(20) NOT NULL,
  sigla VARCHAR(5) NOT NULL,
@@ -10,7 +10,8 @@ CREATE TABLE Associacao (
  numeroPagamento INT NOT NULL,
  numeroOficio INT NOT NULL,
  dataOficio TIMESTAMP NOT NULL,
- temAcesso CHAR(1) NOT NULL DEFAULT 'F' CHECK (temAcesso IN ('T', 'F'))
+ temAcesso CHAR(1) NOT NULL DEFAULT 'F' CHECK (temAcesso IN ('T', 'F')),
+ senha VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE Atleta (
@@ -36,19 +37,13 @@ ALTER TABLE LocalCompeticao ADD CONSTRAINT PK_LocalCompeticao PRIMARY KEY (ender
 CREATE TABLE Competicao (
  nome VARCHAR(50) NOT NULL,
  dataCompeticao TIMESTAMP NOT NULL,
- endereco VARCHAR(300) NOT NULL
+ endereco VARCHAR(300) NOT NULL,
+ tipoPiscina VARCHAR(5) NOT NULL CHECK (tipoPiscina IN ('50M', '25M'))
 );
 
 ALTER TABLE Competicao ADD CONSTRAINT PK_Competicao PRIMARY KEY (dataCompeticao,endereco);
 
-CREATE TABLE Pessoa (
- matricula INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY (START WITH 1000000, INCREMENT BY 1),
- senha VARCHAR(15) NOT NULL,
- tipo VARCHAR(20) NOT NULL
-);
-
 CREATE TABLE Prova (
- tipoPiscina VARCHAR(5) NOT NULL CHECK (tipoPiscina IN ('50M', '25M')),
  nomeProva VARCHAR(30) NOT NULL CHECK (nomeProva IN ('50L', '100L', '200L', '400L',
  '800L', '1500L', '50C', '100C', '200C', '50P', '100P', '200P', '50B', '100B', '200B',
  '200M', '400M')),
@@ -71,19 +66,19 @@ CREATE TABLE AtletaProva (
  pontuacao INT
 );
 
-ALTER TABLE Associacao ADD CONSTRAINT FK_AssociacaoPessoa FOREIGN KEY (matriculaAssociacao) REFERENCES Pessoa (matricula);
 ALTER TABLE Atleta ADD CONSTRAINT FK_Atleta_0 FOREIGN KEY (matriculaAssociacao) REFERENCES Associacao (matriculaAssociacao);
 ALTER TABLE Competicao ADD CONSTRAINT FK_Competicao_0 FOREIGN KEY (endereco) REFERENCES LocalCompeticao (endereco);
 
-ALTER TABLE Prova ADD CONSTRAINT PK_Prova PRIMARY KEY (tipoPiscina, nomeProva,classe,categoria,dataCompeticao,endereco);
+ALTER TABLE Prova ADD CONSTRAINT PK_Prova PRIMARY KEY (nomeProva,classe,categoria,dataCompeticao,endereco);
 ALTER TABLE Prova ADD CONSTRAINT FK_ProvaCompeticao FOREIGN KEY (dataCompeticao,endereco) REFERENCES Competicao (dataCompeticao,endereco);
 
-ALTER TABLE AtletaProva ADD CONSTRAINT PK_AtletaProva PRIMARY KEY (tipoPiscina, nomeProva,classe,categoria, dataCompeticao, endereco,matriculaAtleta);
+ALTER TABLE AtletaProva ADD CONSTRAINT PK_AtletaProva PRIMARY KEY (nomeProva,classe,categoria, dataCompeticao, endereco,matriculaAtleta);
 ALTER TABLE AtletaProva ADD CONSTRAINT FK_AtletaProvaAtleta FOREIGN KEY (matriculaAtleta) REFERENCES Atleta (matriculaAtleta);
-ALTER TABLE AtletaProva ADD CONSTRAINT FK_AtletaProvaProva FOREIGN KEY (tipoPiscina, nomeProva,classe,categoria, dataCompeticao, endereco) REFERENCES Prova (tipoPiscina, nomeProva,classe,categoria, dataCompeticao, endereco);
+ALTER TABLE AtletaProva ADD CONSTRAINT FK_AtletaProvaProva FOREIGN KEY (nomeProva,classe,categoria, dataCompeticao, endereco) REFERENCES Prova (nomeProva,classe,categoria, dataCompeticao, endereco);
 
-
-INSERT INTO COMP3.PESSOA (SENHA, TIPO) values ('12345', 'DIRETOR');
+INSERT INTO COMP3.associacao (nome, telefone, sigla, endereco,
+	numeroPagamento, numeroOficio, dataOficio, temAcesso, senha) 
+	values ('Associacao', '35279464', 'GGG', 'Associacao, 54', 34666, 25342, '2018-01-09 10:30:00.0', 'T', '123456');
 --INSERT INTO COMP3.PESSOA values ('123', 'DIRETOR');
 
 --insert into comp3.localcompeticao values ('Barra', 'BarraPool', 'T', 'T'), ('Piedade', 'PiedadePool', 'F', 'F');
