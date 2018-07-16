983d2105c2d89585cd63f1f7ab3948c16b8110b9
Victor Diniz <emaildovictordiniz@gmail.com>
2018-07-15 22:07:01 -0300

    Teste de Listar Associações

---
+++ Comp3/src/br/sisfarj/ccomp/test/funcional/TesteListarAssociacao.java
@@ -0,0 +1,46 @@
+package br.sisfarj.ccomp.test.funcional;
+
+import java.sql.ResultSet;
+
+import org.junit.Test;
+
+import br.sisfarj.ccomp.dominio.AssociacaoMT;
+import br.sisfarj.ccomp.dominio.adapter.ResultSetAdapter;
+import br.sisfarj.ccomp.dominio.exceptions.NaoHaAssociacaoException;
+import br.sisfarj.ccomp.gateways.AssociacaoGateway;
+
+public class TesteListarAssociacao extends TesteFuncional{
+	
+	public TesteListarAssociacao(String metodo) {
+		super(metodo);
+	}
+	
+	@Test
+	public void testFiliarAssociacaoValida () {
+		try {
+			AssociacaoGateway associacaoGateway = new AssociacaoGateway();
+			ResultSet rs = associacaoGateway.listarTodas();
+			AssociacaoMT associacaoMT = new AssociacaoMT(rs);
+			
+			ResultSetAdapter rsa;
+			rsa = associacaoMT.listarTodas();
+		} catch (NaoHaAssociacaoException e) {
+			
+			fail("Erro ao listar associações: " + e.toString());
+			
+		} catch (Throwable t){
+			  //do nothing since other exceptions are OK
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		try {
+			new TesteListarAssociacao("listarAssociacao").runTest();
+		} catch (Throwable e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
